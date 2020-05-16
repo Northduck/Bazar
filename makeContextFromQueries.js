d9ff30a43737b3d4d85a97ea7987f39cbdeeb272
Denisov Daniil <den23091998@gmail.com>
2020-05-16 14:45:59 +0300

    Added catching error on unhandled promises

--- makeContextFromQueries.js
+++ makeContextFromQueries.js
@@ -1,41 +1,43 @@
 function checkQueriesMas(queries){
-    if(!Array.isArray(queries)){
+  if(!Array.isArray(queries)){
+    return false;
+  }
+  for(var i=0;i<queries.length;i++){
+    if(typeof queries[i].queryValue!="string"||typeof queries[i].contextVarName!="string"){
       return false;
     }
-    for(var i=0;i<queries.length;i++){
-      if(typeof queries[i].queryValue!="string"||typeof queries[i].contextVarName!="string"){
-        return false;
-      }
-    }
-    return true;
+  }
+  return true;
 }
 var makeContext=(queriesMas, dbClient, callback)=>{
-  if(!checkQueriesMas(queriesMas)){
-    return;
+if(!checkQueriesMas(queriesMas)){
+  return;
+}
+var promMas=[];
+var context={};
+queriesMas.forEach(queryObj=>{
+    promMas.push(new Promise((resolve, reject)=>{
+      dbClient.query(queryObj.queryValue,(err,res)=>{
+          if (err){
+              reject(err);
+          }
+          if(!queryObj.queryFunc){
+              resolve(res.rows);
+          }else{
+            queryObj.queryFunc(res.rows,funcResult=>{
+              resolve(funcResult);
+            })
+          }
+        });                
+    }));
+});
+Promise.all(promMas).then(queriesResult=>{
+  for(var i=0;i<queriesResult.length;i++){
+    context[queriesMas[i].contextVarName]=queriesResult[i];
   }
-  var promMas=[];
-  var context={};
-  queriesMas.forEach(queryObj=>{
-      promMas.push(new Promise((resolve, reject)=>{
-        dbClient.query(queryObj.queryValue,(err,res)=>{
-              if (err){
-                  reject(err);
-              }
-              if(!queryObj.queryFunc){
-                  resolve(res.rows);
-              }else{
-                queryObj.queryFunc(res.rows,funcResult=>{
-                  resolve(funcResult);
-                })
-              }
-          });                
-      }));
-  });
-  Promise.all(promMas).then(queriesResult=>{
-    for(var i=0;i<queriesResult.length;i++){
-      context[queriesMas[i].contextVarName]=queriesResult[i];
-    }
-    callback(context);
-  });
+  callback(context);
+}).catch(err=>{
+  console.log("context Error", err);
+});
 }
 module.exports=makeContext;
